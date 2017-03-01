d3a9e11d9b48c9ab698e1d4b13ed6aa744263990
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-03-01 15:45:00 -0300

    Sparklines added

---
+++ src/components/chart.js
@@ -0,0 +1,16 @@
+/**
+ * Created by lucas on 3/1/17.
+ */
+import React from 'react';
+import {Sparklines, SparklinesLine} from 'react-sparklines';
+
+export default (props) => {
+
+    return (
+        <div>
+            <Sparklines heigth={120} width={180} data={props.data}>
+                <SparklinesLine color={props.color} />
+            </Sparklines>
+        </div>
+    );
+};
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,16 +3,24 @@
  */
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart';
+
+const getTemperature = weather => weather.main.temp;
 
 class WeatherList extends Component {
 
     renderWeather(cityData) {
 
         const name = cityData.city.name;
+        const temps = cityData.list.map(getTemperature);
+        console.log(temps)
 
         return (
             <tr key={name}>
                 <td>{name}</td>
+                <td>
+                    <Chart data={temps} color="orange" />
+                </td>
             </tr>
         );
     }
