87e73cf0ab582b8b075a161aeb24281f5110e3b2
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-03-01 14:14:45 -0300

    Making request o open weather forecast

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '45339ef83670d7b6ec5860568e70df98';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+/** @type {String} */
+export function fetchWeather(city) {
+
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,8 +2,11 @@
  * Created by lucas on 3/1/17.
  */
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 
     constructor(props) {
         super(props);
@@ -14,6 +17,10 @@ export default class SearchBar extends Component {
 
     onFormSubmit = (event) => {
         event.preventDefault();
+
+        this.props.fetchWeather(this.state.term);
+
+        this.setState({term: ''});
     };
 
     render() {
@@ -37,3 +44,9 @@ export default class SearchBar extends Component {
         )
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
